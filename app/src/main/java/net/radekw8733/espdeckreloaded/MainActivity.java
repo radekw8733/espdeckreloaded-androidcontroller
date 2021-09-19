5ab42c6d82e89925d86be5e9e83bbc837375660c
Radosław Wolański <radekw8733@gmail.com>
2021-09-19 10:14:44 +0200

    Initial commit

---
+++ app/src/main/java/net/radekw8733/espdeckreloaded/MainActivity.java
@@ -0,0 +1,14 @@
+package net.radekw8733.espdeckreloaded;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
